fa2e6b00fc1af4e1b9462505fd42b9b7c9c83ba2
Leon Böttger <leon.boettger@icloud.com>
2024-11-18 11:42:12 +0100

    Add Application Key Decryption

---
+++ TitanBackup/lskf_hasher.py
@@ -0,0 +1,32 @@
+from binascii import unhexlify
+import pyscrypt
+from private import sample_pin, sample_pin_salt
+
+
+def ascii_to_bytes(string):
+    return string.encode('ascii')
+
+
+def get_lskf_hash(pin, salt):
+    # Parameters
+    data_to_hash = ascii_to_bytes(pin)  # Convert the string to an ASCII byte array
+
+    log_n_cost = 4096  # CPU/memory cost parameter
+    block_size = 8  # Block size
+    parallelization = 1  # Parallelization factor
+    key_length = 32  # Length of the derived key in bytes
+
+    # Perform Scrypt hashing
+    hashed = pyscrypt.hash(
+        password=data_to_hash,
+        salt=unhexlify(salt),
+        N=log_n_cost,
+        r=block_size,
+        p=parallelization,
+        dkLen=key_length
+    )
+
+    return hashed
+
+if __name__ == '__main__':
+   print(get_lskf_hash(sample_pin, sample_pin_salt).hex())
