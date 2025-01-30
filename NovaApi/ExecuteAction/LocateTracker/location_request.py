99654f1169bf13e6bd21a76e0d147637819f04aa
Leon Böttger <leon.boettger@icloud.com>
2025-01-30 13:22:40 +0100

    Typo

--- NovaApi/ExecuteAction/LocateTracker/location_request.py
+++ NovaApi/ExecuteAction/LocateTracker/location_request.py
@@ -43,7 +43,7 @@ def get_location_data_for_device(canonic_device_id):
         device_update = parse_device_update_protobuf(response)
 
         if device_update.fcmMetadata.requestUuid == request_uuid:
-            print("[LocationRequest] Location request successful. Reponse:")
+            print("[LocationRequest] Location request successful. Response:")
             result = parse_device_update_protobuf(response)
             print_device_update_protobuf(response)
         else:
