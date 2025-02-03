eb99c241e2e20cffb854ac9e1c6cbecf5bc17ac1
Leon Böttger <leon.boettger@icloud.com>
2025-02-03 15:01:56 +0100

    Fix Auth if running for longer periods of time

--- Auth/adm_token_retrieval.py
+++ Auth/adm_token_retrieval.py
@@ -6,18 +6,8 @@
 from Auth.token_retrieval import request_token
 from Auth.username_provider import get_username
 
-cached_adm_token = None
-
 def get_adm_token(username):
-
-    global cached_adm_token
-
-    if cached_adm_token:
-        print("[AdmTokenRetrieval] Using Cached App Token.")
-        return cached_adm_token
-
-    cached_adm_token = request_token(username, "android_device_manager")
-    return cached_adm_token
+    return request_token(username, "android_device_manager")
 
 
 if __name__ == '__main__':
--- Auth/spot_token_retrieval.py
+++ Auth/spot_token_retrieval.py
@@ -6,18 +6,8 @@
 from Auth.token_retrieval import request_token
 from Auth.username_provider import get_username
 
-cached_spot_token = None
-
 def get_spot_token(username):
-
-    global cached_spot_token
-
-    if cached_spot_token:
-        print("[SpotTokenRetrieval] Using Cached Spot Token.")
-        return cached_spot_token
-
-    cached_spot_token = request_token(username, "spot", True)
-    return cached_spot_token
+    return request_token(username, "spot", True)
 
 if __name__ == '__main__':
     print(get_spot_token(get_username()))
